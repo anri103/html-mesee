03049cf76a491c71751b9ddacd6baa1cdcee1055
anri103 <ianrianri@gmail.com>
2023-06-28 15:26:25 +0300

    add modals

--- js/main.js
+++ js/main.js
@@ -1,25 +1,10 @@
-const chat = document.getElementById("chat");
-const success = document.getElementById("success");
-const chat_button = document.getElementById("chat_button");
-chat_button.addEventListener("click", e => {
-  e.preventDefault();
-  chat.classList.add("hidden_chat");
-  success.classList.add("active_success");
-});
 document.addEventListener("DOMContentLoaded", () => {
-  const inputElement = document.querySelector("#chat_phone");
+  const inputElement = document.querySelector(".mask_phone");
   const maskOptions = {
     mask: "(000)-000-00-00"
   };
   IMask(inputElement, maskOptions);
 });
-const hide_button = document.querySelector(".hide_button");
-if (hide_button) {
-  hide_button.addEventListener("click", () => {
-    hide_button.classList.contains("hide_button_active") ? hide_button.childNodes[1].innerHTML = "смотреть все" : hide_button.childNodes[1].innerHTML = "скрыть";
-    hide_button.classList.toggle("hide_button_active");
-  });
-}
 
 const feedbackSwiper = new Swiper(".feedbackSwiper", {
   slidesPerView: 1,
